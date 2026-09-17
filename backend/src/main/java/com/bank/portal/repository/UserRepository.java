9721433968b17c8542bfcc60bc9575ecd14a9bd5
sainikajal6708 <sainikajal6708@gmail.com>
2026-09-17 15:48:32 +0530

    Create UserRepository.java

---
+++ backend/src/main/java/com/bank/portal/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.bank.portal.repository;
+import com.bank.portal.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
